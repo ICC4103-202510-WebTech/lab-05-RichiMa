3101995de2e82463bbebfb143c3f5c34fa8009d7
RichiMa <rimadariaga@miuandes.cl>
2025-05-16 15:01:28 -0400

    lab08(edit,update,etccc)

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,6 +6,7 @@ class ChatsController < ApplicationController
   
     def show 
       @chat = Chat.find(params[:id])
+      @messages = @chat.messages
     end
   
     def new
@@ -13,16 +14,31 @@ class ChatsController < ApplicationController
       @users = User.all
     end
   
+    def edit
+      @chat = Chat.find(params[:id])
+      @users = User.all
+    end
+
     def create
       @chat = Chat.new(chat_params)
       if @chat.save
-        redirect_to @chat
+        redirect_to @chat, notice: 'El chat se creo exitosamente! :D'
       else
         @users = User.all
         render :new
       end
     end
   
+    def update
+      @chat = Chat.find(params[:id])
+      @users = User.all
+      if @chat.update(chat_params)
+        redirect_to @chat, notice: 'Se actualizo el chat'
+      else 
+        render :edit
+      end
+    end
+
     private
   
     def chat_params
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,11 +13,15 @@ class MessagesController < ApplicationController
         @chats = Chat.all
         @users = User.all
     end
+
+    def edit
+        @message = Message.find(params[:id])
+    end
   
     def create 
         @message = Message.new(message_params)
             if @message.save
-                redirect_to @message
+                redirect_to @message, notice: 'Se creo exitosamente el mensaje! :D'
             else
                 @chats = Chat.all
                 @users = User.all
@@ -25,10 +29,18 @@ class MessagesController < ApplicationController
         end
     end
   
+    def update
+        @message = Message.find(params[:id])
+        if @message.update(message_params)
+            redirect_to @message, notice: 'Se actualizo el mensaje'
+        else
+            render :edit
+        end
+    end
+
     private
   
     def message_params
         params.require(:message).permit(:body, :chat_id, :user_id)
     end
   end
-  
